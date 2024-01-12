6cf76dae6b40329056ab61ba4538aed1923c08b7
Glenn Chang <glchang@bcgsc.ca>
2024-01-12 15:26:31 -0800

    add bash script to run snakemake

---
+++ run_snakemake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p upgrade
+#SBATCH --job-name='run_snakemake'
+#SBATCH --mail-type=ALL
+#SBATCH --mem=1G
+#SBATCH -n 1
+
+eval "$(conda shell.bash hook)"
+conda activate snakemake
+
+if [[ $1 == "slurm" ]]; then
+    slurm="--profile=slurm"
+fi
+
+snakemake ${slurm} -k -c 72 --use-singularity --singularity-args "-B /projects,/gsc,/home"  --rerun-incomplete
